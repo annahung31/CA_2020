5a7172691693858183012bdaf936ffc90c4382b9
Anna <fbiannahung@gmail.com>
2020-10-16 12:47:14 +0800

    move into folder

---
+++ hw2/Problems/fibonacci/fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+unsigned long long int fibonacci(int);
+
+
+
+int main () {
+    unsigned long long int ret;
+
+    for (int i = 0; i < 94; i++) {
+        ret = fibonacci(i);
+        printf("%llu\n", ret);
+    }
+
+    return 0;
+}
---
+++ hw2/docker.sh
@@ -0,0 +1,1 @@
+docker run -it -v ~/Documents/2020_Fall/CA_2020:/home ntuca2020/hw2 /bin/bash
