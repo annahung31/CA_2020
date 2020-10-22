8fdac4bae6f588261205ecbeac3807c19910da8e
Anna <fbiannahung@gmail.com>
2020-10-22 16:55:04 +0800

    all positive

--- hw2/Problems/convert/convert.s
+++ hw2/Problems/convert/convert.s
@@ -14,33 +14,42 @@ convert:
     addi t3, x0, 58   #ASCII of 9
     addi t6, x0, 0    #ASCII of end
     
+    addi a6, x0, 10   #10
     addi t5, x0, 0    #loop idx
-
-
-
+    addi a3, x0, 0    #flag of neg
+    addi a4, x0, 0    #init result
+    
+    lb t4, 0(a0)
+    bne t4, t0, Neg
 
     Loop:
         add a2, a0, t5
         lb t4, 0(a2)
-        beq t4, t6, Exit
-        #beq t4,  t0, Pos
-        #bne t4, t0, Neg
+        beq t4, t6, Final
         bge t4, t3, Cha
+        bge t4, t2, Cal
+    Coni:    
         addi t5, t5, 1
         beq x0, x0, Loop    
+
+
+    Cal:
+        sub t4, t4, t2
+        mul a4, a4, a6
+        add a4, a4, t4
+        beq x0, x0, Coni 
 
 
     Cha:
         addi a0, x0, -1
         beq x0, x0, Exit
 
-
-    Pos:
-        addi a0, x0, 1
-        beq x0, x0, Exit
-
     Neg:
-        addi a0, x0, 0
+        addi a3, x0, 1
+        beq x0, x0, Loop
+
+    Final:
+        add a0, x0, a4
         beq x0, x0, Exit
 
 
