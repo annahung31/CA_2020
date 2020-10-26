7cb34fa220c0e0edd616690f843f3cc1fc6d39a3
Anna <fbiannahung@gmail.com>
2020-10-26 14:53:51 +0800

    temp

--- hw2/Problems/matrix/matrix.c
+++ hw2/Problems/matrix/matrix.c
@@ -28,7 +28,36 @@ int main () {
 
 
     asm volatile ("rdcycle %0" : "=r" (start));
-    matrix_mul(A, B, C);
+    // matrix_mul(A, B, C);
+    // int i, j;
+    // for (int i = 0; i < SIZE; i++){
+    //     for (int j = 0; j < SIZE; j++){
+    //         for (int k = 0; k < SIZE; k++){
+    //             C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD;
+    //             printf("%f\n", C[i][j]);
+    //         }
+    //     }
+    // }
+        
+            
+    unsigned short sum;
+    int bsize=25;
+    int en = bsize * (SIZE/bsize);
+    for (int kk = 0; kk < en; kk += bsize) {
+        for (int jj = 0; jj < en; jj += bsize) {
+	        for (int i = 0; i < SIZE; i++) {
+		        for (int j = jj; j < jj+bsize; j++) { // updated 2006/12/19
+		            sum = C[i][j];
+		            for (int k = kk; k < kk+bsize; k++) {
+		                sum = ( sum + A[i][k] * B[k][j]) % MOD;
+		            }
+		            C[i][j] = sum;
+                    
+		        }
+	        }
+        }
+    }
+
     asm volatile ("rdcycle %0" : "=r" (end));
 
 
@@ -42,9 +71,15 @@ int main () {
             for (int k = 0; k < SIZE; k++)
                 check[i][j] = (check[i][j] + A[i][k] * B[k][j]) % MOD;
 
-    for (int i = 0; i < SIZE; i++)
-        for (int j = 0; j < SIZE; j++)
+    for (int i = 0; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++){
+            // printf("%f, %f \n",check[i][j], C[i][j]);
             assert(check[i][j] == C[i][j]);
+        }
+    }
+
+
+
 
     printf("Took %llu cycles\n", end - start);
 }
