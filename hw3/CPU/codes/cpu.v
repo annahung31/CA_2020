7834056f642b244468a78412e6687f799c54325e
Anna <fbiannahung@gmail.com>
2020-11-27 08:06:56 +0800

    load correct

--- hw3/CPU/codes/cpu.v
+++ hw3/CPU/codes/cpu.v
@@ -26,6 +26,7 @@ module cpu #( // Do not modify interface
     integer            i;
     reg [63:0]         pc_r, pc_w;
     reg [3:0]          cs, ns;
+    reg [3:0]          dcs, dns;
     reg [DATA_W-1 : 0] reg_file[0:31];  //reg_file[0].size=DATA_W
     reg [DATA_W-1 : 0] reg_file_w[0:31];
     reg                o_i_valid_r, o_i_valid_w;
@@ -40,20 +41,26 @@ module cpu #( // Do not modify interface
     reg                o_d_MemWrite_r, o_d_MemWrite_w;
     reg                over_r, over_w;
     reg                shift_temp;
+    reg                data_count;
+    reg [INST_W-1 : 0] i_inst_temp;
     wire [6:0]         opcode;
+    wire [5:0]         rdd;
 
 
+
+
+    assign rdd      = i_i_inst[11:7];
     assign opcode   = i_i_inst[6:0];
     assign o_i_valid_addr = o_i_valid_r;
     assign o_finish = o_finish_r;
     assign o_i_addr = o_i_addr_r;
-    
+
     assign o_d_addr = o_d_addr_r;
     assign o_d_data = o_d_data_r;
     assign o_d_MemWrite = o_d_MemWrite_r;
     assign o_d_MemRead = o_d_MemRead_r;
 
-    // count 5 cycles to wait for instr mem.
+    // count 15 cycles to wait for instr mem.
     always @(*) begin
         case (cs) //current state
             0: ns = 1;  // ns: next state
@@ -73,6 +80,10 @@ module cpu #( // Do not modify interface
             14: ns = 15;
             15: ns = 0;
         endcase
+        if (i_i_valid_inst) begin
+            i_inst_temp = i_i_inst;
+            
+        end
     end
 
 
@@ -109,181 +120,188 @@ module cpu #( // Do not modify interface
         if (cs == 6) begin
 
             // LD
-            if (i_i_inst[6:0] == 7'b0000011 
-                && i_i_inst[14:12] == 3'b011) begin
+            if (i_inst_temp[6:0] == 7'b0000011 
+             && i_inst_temp[14:12] == 3'b011) begin
                 
-                imm[11:0] = i_i_inst[31:20];
-                rs1 = i_i_inst[19:15];
+                imm[11:0] = i_inst_temp[31:20];
+                rs1 = i_inst_temp[19:15];
                 
                 rs1_value = reg_file[rs1];
-                rd = i_i_inst[11:7]; 
+                rd = i_inst_temp[11:7]; 
                 
                 o_d_addr_w = {{52{imm[11]}}, imm[11:0]} + rs1_value;  //addr
-                o_d_data_w = rs2;                                         // data
+                                           
                 o_d_MemRead_w = 1;
+                // o_d_MemWrite_w = 0;
+                data_count = 1;
+                // $display("%x", i_d_data);
+                
             end
 
 
-
-            // ADDI
-            else if (i_i_inst[6:0] == 7'b0010011 
-                && i_i_inst[14:12] == 3'b000) begin
-                
-                rs1 = i_i_inst[19:15];
-                rd = i_i_inst[11:7];  // 11:7
-                // $display("%d %x %x %x", opcode, rs1, reg_file[rs1], rs1_value);
-                rs1_value = reg_file[rs1];
-                imm[11:0] = i_i_inst[31:20];
-                reg_file_w[rd] = {{52{imm[11]}}, imm[11:0]} + rs1_value; 
-                o_d_MemWrite_w = 0;
-            end            
-
-
             // SD
-            else if (i_i_inst[6:0] == 7'b0100011 
-                && i_i_inst[14:12] == 3'b011) begin
-                imm[4:0] = i_i_inst[11:7];
-                imm[11:5] = i_i_inst[31:25];
+            else if (i_inst_temp[6:0] == 7'b0100011 
+                && i_inst_temp[14:12] == 3'b011) begin
+                imm[4:0] = i_inst_temp[11:7];
+                imm[11:5] = i_inst_temp[31:25];
 
-                rs1 = i_i_inst[19:15];
+                rs1 = i_inst_temp[19:15];
                 rs1_value = reg_file[rs1];
-                rs2 = i_i_inst[24:20];
+                rs2 = i_inst_temp[24:20];
                 // $display("%d %x %x %x %x %x", opcode, rs1, rs1_value, {{52{imm[11]}}, imm[11:0]}, o_d_addr_w, o_d_data_w);
                 
 
                 o_d_addr_w = {{52{imm[11]}}, imm[11:0]} + rs1_value;  //addr
                 o_d_data_w = reg_file[rs2];                                         // data
                 o_d_MemWrite_w = 1;
+                // o_d_MemRead_w = 0;
+                data_count = 1;
+                
                 
                 
             end
 
 
+            // ADDI
+            else if (i_inst_temp[6:0] == 7'b0010011 
+                && i_inst_temp[14:12] == 3'b000) begin
+                
+                rs1 = i_inst_temp[19:15];
+                rd = i_inst_temp[11:7];  // 11:7
+                // $display("%d %x", opcode, {{52{imm[11]}}, imm[11:0]} );
+                rs1_value = reg_file[rs1];
+                imm[11:0] = i_inst_temp[31:20];
+                reg_file_w[rd] = {{52{imm[11]}}, imm[11:0]} + rs1_value; 
+                o_d_MemWrite_w = 0;
+                o_d_MemRead_w = 0;
+            end            
+
+
             // OR
-            else if (i_i_inst[6:0] == 7'b0110011 
-                    && i_i_inst[31:25] == 7'b0000000
-                    && i_i_inst[14:12] == 3'b110) begin
-                rd = i_i_inst[11:7];
-                rs1 = i_i_inst[19:15];
-                rs2 = i_i_inst[24:20];
+            else if (i_inst_temp[6:0] == 7'b0110011 
+                    && i_inst_temp[31:25] == 7'b0000000
+                    && i_inst_temp[14:12] == 3'b110) begin
+                rd = i_inst_temp[11:7];
+                rs1 = i_inst_temp[19:15];
+                rs2 = i_inst_temp[24:20];
                 reg_file_w[rd] = reg_file[rs1] | reg_file[rs2];
 
             end
 
             // AND
-            else if (i_i_inst[6:0] == 7'b0110011 
-                    && i_i_inst[31:25] == 7'b0000000 
-                    && i_i_inst[14:12] == 3'b111) begin
-                rd = i_i_inst[11:7];
-                rs1 = i_i_inst[19:15];
-                rs2 = i_i_inst[24:20];
+            else if (i_inst_temp[6:0] == 7'b0110011 
+                    && i_inst_temp[31:25] == 7'b0000000 
+                    && i_inst_temp[14:12] == 3'b111) begin
+                rd = i_inst_temp[11:7];
+                rs1 = i_inst_temp[19:15];
+                rs2 = i_inst_temp[24:20];
                 reg_file_w[rd] = reg_file[rs1] & reg_file[rs2];
             end
 
             // XOR
-            else if (i_i_inst[6:0] == 7'b0110011 
-                    && i_i_inst[31:25] == 7'b0000000 
-                    && i_i_inst[14:12] == 3'b100) begin
-                rd = i_i_inst[11:7];
-                rs1 = i_i_inst[19:15];
-                rs2 = i_i_inst[24:20];
+            else if (i_inst_temp[6:0] == 7'b0110011 
+                    && i_inst_temp[31:25] == 7'b0000000 
+                    && i_inst_temp[14:12] == 3'b100) begin
+                rd = i_inst_temp[11:7];
+                rs1 = i_inst_temp[19:15];
+                rs2 = i_inst_temp[24:20];
                 reg_file_w[rd] = reg_file[rs1] ^ reg_file[rs2];
             end
 
 
             // ADD
-            else if (i_i_inst[6:0] == 7'b0110011 && i_i_inst[31:25] == 7'b0000000) begin
-                rd = i_i_inst[11:7];
-                rs1 = i_i_inst[19:15];
-                rs2 = i_i_inst[24:20];
+            else if (i_inst_temp[6:0] == 7'b0110011 && i_inst_temp[31:25] == 7'b0000000) begin
+                rd = i_inst_temp[11:7];
+                rs1 = i_inst_temp[19:15];
+                rs2 = i_inst_temp[24:20];
                 reg_file_w[rd] = reg_file[rs1] + reg_file[rs2];
             end
 
             // SUB
-            else if (i_i_inst[6:0] == 7'b0110011 && i_i_inst[31:25] == 7'b0100000) begin
-                rd = i_i_inst[11:7];
-                rs1 = i_i_inst[19:15];
-                rs2 = i_i_inst[24:20];
+            else if (i_inst_temp[6:0] == 7'b0110011 && i_inst_temp[31:25] == 7'b0100000) begin
+                rd = i_inst_temp[11:7];
+                rs1 = i_inst_temp[19:15];
+                rs2 = i_inst_temp[24:20];
                 reg_file_w[rd] = reg_file[rs1] - reg_file[rs2];
             end
 
 
             // XORI
-            else if (i_i_inst[6:0] == 0010011 
-                  && i_i_inst[14:12] == 3'b100) begin
-                rd = i_i_inst[11:7];
-                rs1 = i_i_inst[19:15];
-                imm[11:0] = i_i_inst[31:20];
+            else if (i_inst_temp[6:0] == 0010011 
+                  && i_inst_temp[14:12] == 3'b100) begin
+                rd = i_inst_temp[11:7];
+                rs1 = i_inst_temp[19:15];
+                imm[11:0] = i_inst_temp[31:20];
                 reg_file_w[rd] = reg_file[rs1] ^ {{52{imm[11]}}, imm[11:0]};
             end
             
             // ORI
-            else if (i_i_inst[6:0] == 0010011 
-                  && i_i_inst[14:12] == 3'b100) begin
-                rd = i_i_inst[11:7];
-                rs1 = i_i_inst[19:15];
-                imm[11:0] = i_i_inst[31:20];
+            else if (i_inst_temp[6:0] == 0010011 
+                  && i_inst_temp[14:12] == 3'b100) begin
+                rd = i_inst_temp[11:7];
+                rs1 = i_inst_temp[19:15];
+                imm[11:0] = i_inst_temp[31:20];
                 reg_file_w[rd] = reg_file[rs1] | {{52{imm[11]}}, imm[11:0]};
             end
 
 
             // ANDI
-            else if (i_i_inst[6:0] == 0010011 
-                  && i_i_inst[14:12] == 3'b100) begin
-                rd = i_i_inst[11:7];
-                rs1 = i_i_inst[19:15];
-                imm[11:0] = i_i_inst[31:20];
+            else if (i_inst_temp[6:0] == 0010011 
+                  && i_inst_temp[14:12] == 3'b100) begin
+                rd = i_inst_temp[11:7];
+                rs1 = i_inst_temp[19:15];
+                imm[11:0] = i_inst_temp[31:20];
                 reg_file_w[rd] = reg_file[rs1] & {{52{imm[11]}}, imm[11:0]};
             end
 
 
             // SLLI
-            else if (i_i_inst[6:0] == 7'b0010011 
-                  && i_i_inst[31:25] == 7'b0000000
-                  && i_i_inst[14:12] == 3'b001) begin
-                rd = i_i_inst[11:7];
-                rs1 = i_i_inst[19:15];
-                rs2 = i_i_inst[24:20];  // shamt
+            else if (i_inst_temp[6:0] == 7'b0010011 
+                  && i_inst_temp[31:25] == 7'b0000000
+                  && i_inst_temp[14:12] == 3'b001) begin
+                rd = i_inst_temp[11:7];
+                rs1 = i_inst_temp[19:15];
+                rs2 = i_inst_temp[24:20];  // shamt
                 reg_file_w[rd] = reg_file[rs1] << reg_file[rs2];
 
             end
 
             // SRLI
-            else if (i_i_inst[6:0] == 7'b0010011 
-                  && i_i_inst[31:25] == 7'b0000000
-                  && i_i_inst[14:12] == 3'b101) begin
-                rd = i_i_inst[11:7];
-                rs1 = i_i_inst[19:15];
-                rs2 = i_i_inst[24:20];  // shamt
+            else if (i_inst_temp[6:0] == 7'b0010011 
+                  && i_inst_temp[31:25] == 7'b0000000
+                  && i_inst_temp[14:12] == 3'b101) begin
+                rd = i_inst_temp[11:7];
+                rs1 = i_inst_temp[19:15];
+                rs2 = i_inst_temp[24:20];  // shamt
                 reg_file_w[rd] = reg_file[rs1] >> reg_file[rs2];
             end
 
 
             // BEQ
 
-            else if (i_i_inst[6:0] == 7'b1100011 
-                  && i_i_inst[14:12] == 3'b000) begin
-                      if (i_i_inst[19:15] == i_i_inst[24:20])
+            else if (i_inst_temp[6:0] == 7'b1100011 
+                  && i_inst_temp[14:12] == 3'b000) begin
+                      if (i_inst_temp[19:15] == i_inst_temp[24:20])
                             pc_w = pc_r + 
-                                    ({i_i_inst[31], i_i_inst[7], i_i_inst[30:25],i_i_inst[11:8]} << 1);
+                                    ({i_inst_temp[31], i_inst_temp[7], i_inst_temp[30:25],i_inst_temp[11:8]} << 1);
                 
             end
 
 
             // BNE
 
-            else if (i_i_inst[6:0] == 7'b1100011 
-                  && i_i_inst[14:12] == 3'b001) begin
-                      if (i_i_inst[19:15] != i_i_inst[24:20])
+            else if (i_inst_temp[6:0] == 7'b1100011 
+                  && i_inst_temp[14:12] == 3'b001) begin
+                      if (i_inst_temp[19:15] != i_inst_temp[24:20])
                             pc_w = pc_r + 
-                                ({i_i_inst[31], i_i_inst[7], i_i_inst[30:25],i_i_inst[11:8]} << 1);
+                                ({i_inst_temp[31], i_inst_temp[7], i_inst_temp[30:25],i_inst_temp[11:8]} << 1);
                 
             end
 
 
 
 
-            else if (i_i_inst[6:0] == 7'b1111111) begin
+            else if (i_inst_temp[6:0] == 7'b1111111) begin
                 o_finish_w = 1;
             end        
         end
@@ -298,12 +316,49 @@ module cpu #( // Do not modify interface
     end
 
 
+    // clock for data mem
+    always @(*) begin
+        case (dcs) //current state
+            0: dns = (data_count)? 1 : 0;  // dns: next state
+            1: begin
+                if (o_d_MemWrite_r) begin
+                    o_d_MemWrite_w = 0;
+                end
+                else if(o_d_MemRead_r) begin
+                    o_d_MemRead_w = 0;
+                end
+                dns = 2;
+            end
+            2: dns = 3;
+            3: dns = 4;
+            4: dns = 5;
+            5: dns = 6;
+            6: dns = 7;
+            7: dns = 8;
+            8: dns = 9;
+            9: dns = 10;
+            10: begin
+                dns = 0;
+                data_count = 0;
+            end
+        endcase
+
+        if (i_d_valid_data) begin
+            
+            reg_file[i_inst_temp[11:7]] = i_d_data;
+            
+
+        end
+    end
+
+
 
     // sequential
     always @(posedge i_clk or negedge i_rst_n) begin
         if(~i_rst_n) begin
             pc_r           <= 0;   
             cs             <= 0;
+            dcs            <= 0;
             o_i_valid_r    <= 0;
             o_finish_r     <= 0;
             o_i_addr_r     <= 0;
@@ -311,7 +366,8 @@ module cpu #( // Do not modify interface
             o_d_addr_r     <= 0;
             o_d_MemWrite_r <= 0;
             o_d_MemRead_r  <= 0;
-            
+            data_count     <= 0;
+            i_inst_temp    <= 64'b0;
 
             for (i=0; i < 32; i=i+1)
                 reg_file[i] <= 64'b0;
@@ -320,6 +376,7 @@ module cpu #( // Do not modify interface
             
             pc_r           <= pc_w;
             cs             <= ns;
+            dcs            <= dns;
             o_i_valid_r    <= o_i_valid_w;
             o_finish_r     <= o_finish_w;
             o_i_addr_r     <= o_i_addr_w;
