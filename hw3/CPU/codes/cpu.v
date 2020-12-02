f25452e4e6ec71fa7a57c4e00302d07eb3c1136c
Anna <fbiannahung@gmail.com>
2020-12-02 16:32:15 +0800

    correct but weird

--- hw3/CPU/codes/cpu.v
+++ hw3/CPU/codes/cpu.v
@@ -25,13 +25,14 @@ module cpu #( // Do not modify interface
 
     integer            i;
     reg [63:0]         pc_r, pc_w;
+    reg [11:0]         pc_debug;
     reg [3:0]          cs, ns;
     reg [3:0]          dcs, dns;
     reg [DATA_W-1 : 0] reg_file[0:31];  //reg_file[0].size=DATA_W
     reg [DATA_W-1 : 0] reg_file_w[0:31];
     reg                o_i_valid_r, o_i_valid_w;
     reg [4 : 0]        rd, rs1, rs2;
-    reg [DATA_W-1 : 0] rs1_value;
+    reg [DATA_W-1 : 0] rs1_value, rs2_value;
     reg [11 : 0]       imm;
     reg                o_finish_r, o_finish_w;
     reg [ADDR_W-1 : 0] o_i_addr_r, o_i_addr_w;
@@ -43,6 +44,8 @@ module cpu #( // Do not modify interface
     reg                shift_temp;
     reg                data_count;
     reg [INST_W-1 : 0] i_inst_temp;
+    reg                is_branch_r, is_branch_w;
+    reg [ADDR_W-1 : 0] b_target;
     wire [6:0]         opcode;
     wire [5:0]         rdd;
 
@@ -50,7 +53,7 @@ module cpu #( // Do not modify interface
 
 
     assign rdd      = i_i_inst[11:7];
-    assign opcode   = i_i_inst[6:0];
+    assign opcode   = i_inst_temp[6:0];
     assign o_i_valid_addr = o_i_valid_r;
     assign o_finish = o_finish_r;
     assign o_i_addr = o_i_addr_r;
@@ -88,37 +91,38 @@ module cpu #( // Do not modify interface
 
 
 
-    always @(*) begin
-        if (cs == 15) begin
+    // always @(*) begin
+    //     if (cs == 15) begin
+    //         if (is_branch_w) begin
+    //             pc_w = pc_r;
+    //             o_i_addr_w = pc_r;
+    //         end
+    //         else begin
+    //             pc_w = pc_r + 4;
+    //             o_i_addr_w = pc_r;
+    //         end
             
-            pc_w = pc_r + 4;
-            o_i_addr_w = pc_r;
-            o_i_valid_w = 1;
+    //         o_i_valid_w = 1;
 
-        end 
-
-        // else if (cs == 0) begin
-        //     o_i_valid_w = 1;
-        // end
+    //     end 
         
-        else begin
+    //     else begin
+    //     	o_i_valid_w = 0; 
+    //         // pc_w = pc_r;
+    //         o_i_addr_w = pc_r;
             
-        	o_i_valid_w = 0; 
-            pc_w = pc_r;
-            o_i_addr_w = pc_w;
+    //     end
 
-            
-        end
-
-    end
+    // end
 
     // check if instr is fetched.
     always @( *) begin
+         
         for (i=0; i < 32; i=i+1)
             reg_file_w[i] = reg_file[i];
-
+        
         if (cs == 6) begin
-
+            is_branch_w = 0;
             // LD
             if (i_inst_temp[6:0] == 7'b0000011 
              && i_inst_temp[14:12] == 3'b011) begin
@@ -275,10 +279,17 @@ module cpu #( // Do not modify interface
 
             else if (i_inst_temp[6:0] == 7'b1100011 
                   && i_inst_temp[14:12] == 3'b000) begin
-                      if (i_inst_temp[19:15] == i_inst_temp[24:20])
-                            pc_w = pc_r + 
-                                    ({i_inst_temp[31], i_inst_temp[7], i_inst_temp[30:25],i_inst_temp[11:8]} << 1);
-                
+                rs1_value = reg_file[i_inst_temp[19:15]];
+                rs2_value = reg_file[i_inst_temp[24:20]];
+                if (reg_file[i_inst_temp[19:15]] == reg_file[i_inst_temp[24:20]]) begin
+                    
+                    pc_debug = ({i_inst_temp[31], i_inst_temp[7], i_inst_temp[30:25],i_inst_temp[11:8]} << 1);
+                    b_target = pc_r + 
+                            ({i_inst_temp[31], i_inst_temp[7], i_inst_temp[30:25],i_inst_temp[11:8]} << 1);
+            
+                    is_branch_w = 1;
+                end
+
             end
 
 
@@ -286,24 +297,50 @@ module cpu #( // Do not modify interface
 
             else if (i_inst_temp[6:0] == 7'b1100011 
                   && i_inst_temp[14:12] == 3'b001) begin
-                      if (i_inst_temp[19:15] != i_inst_temp[24:20])
-                            pc_w = pc_r + 
-                                ({i_inst_temp[31], i_inst_temp[7], i_inst_temp[30:25],i_inst_temp[11:8]} << 1);
+                rs1_value = reg_file[i_inst_temp[19:15]];
+                rs2_value = reg_file[i_inst_temp[24:20]];
+                if (reg_file[i_inst_temp[19:15]] != reg_file[i_inst_temp[24:20]]) begin
+                    pc_debug = ({i_inst_temp[31], i_inst_temp[7], i_inst_temp[30:25],i_inst_temp[11:8]} << 1);
+                    b_target = pc_r + 
+                        ({i_inst_temp[31], i_inst_temp[7], i_inst_temp[30:25],i_inst_temp[11:8]} << 1);
                 
+                    is_branch_w = 1;
+                end 
+                
+                    
+
+                    
             end
-
-
 
 
             else if (i_inst_temp[6:0] == 7'b1111111) begin
                 o_finish_w = 1;
-            end        
+            end
+
+        	o_i_valid_w = 0; 
+            o_i_addr_w = pc_r;        
         end
+
+        else if (cs == 15) begin
+            if (is_branch_r) begin
+                pc_w = b_target-4;
+                o_i_addr_w = b_target-4;
+            end
+            else begin
+                pc_w = pc_r + 4;
+                o_i_addr_w = pc_r;
+            end
+            o_i_valid_w = 1;
+        
+        end
+
         else begin
             o_d_MemWrite_w = 0;
             o_finish_w = 0;
             o_d_data_w = 0;
             o_d_MemRead_w = 0;
+        	o_i_valid_w = 0; 
+            o_i_addr_w = pc_r;  
         end
             
             
@@ -350,7 +387,8 @@ module cpu #( // Do not modify interface
     // sequential
     always @(posedge i_clk or negedge i_rst_n) begin
         if(~i_rst_n) begin
-            pc_r           <= 0;   
+            pc_r           <= 64'b0;   
+            pc_w           <= 64'b0;   
             cs             <= 0;
             dcs            <= 0;
             o_i_valid_r    <= 0;
@@ -362,6 +400,8 @@ module cpu #( // Do not modify interface
             o_d_MemRead_r  <= 0;
             data_count     <= 0;
             i_inst_temp    <= 64'b0;
+            is_branch_r    <= 0;
+            
 
             for (i=0; i < 32; i=i+1)
                 reg_file[i] <= 64'b0;
@@ -378,6 +418,7 @@ module cpu #( // Do not modify interface
             o_d_addr_r     <= o_d_addr_w;
             o_d_MemWrite_r <= o_d_MemWrite_w;
             o_d_MemRead_r  <= o_d_MemRead_w;
+            is_branch_r    <= is_branch_w;
             
             
 
